d90e5537a33c9a6408578dbf5328d29109101693
jahi1998 <13691554018@163.com>
2021-10-21 20:07:36 -0500

    add new button and work now

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,110 +1,96 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import TextField from "@mui/material/TextField";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 const taskList = [
-    {
-        "description": "create task component",
-        "difficulty": 3,
-        "assignees": [
-            "Jake",
-            "Misha"
-        ],
-        "priority": 5,
-        "completed": false
-    },
-    {
-        "description": "Assign tasks button",
-        "difficulty": 3,
-        "assignees": [
-            "Iris",
-            "Carina"
-        ],
-        "priority": 5,
-        "completed": false
-    },
-    {
-        "description": "Task List",
-        "difficulty": 3,
-        "assignees": [
-            "Daniel",
-            "Yijing"
-        ],
-        "priority": 3,
-        "completed": false
-    },
-    {
-        "description": "login",
-        "difficulty": 3,
-        "assignees": [
-            "Jake",
-            "Carina"
-        ],
-        "priority": 2,
-        "completed": false
-    },
-    {
-        "description": "mood tracking",
-        "difficulty": 3,
-        "assignees": [
-            "Misha",
-            "Daniel",
-            "Iris"
-        ],
-        "priority": 1,
-        "completed": false
-    }
-]
-
+  {
+    description: "create task component",
+    difficulty: 3,
+    assignees: ["Jake", "Misha"],
+    priority: 5,
+    completed: false,
+  },
+  {
+    description: "Assign tasks button",
+    difficulty: 3,
+    assignees: ["Iris", "Carina"],
+    priority: 5,
+    completed: false,
+  },
+  {
+    description: "Task List",
+    difficulty: 3,
+    assignees: ["Daniel", "Yijing"],
+    priority: 3,
+    completed: false,
+  },
+  {
+    description: "login",
+    difficulty: 3,
+    assignees: ["Jake", "Carina"],
+    priority: 2,
+    completed: false,
+  },
+  {
+    description: "mood tracking",
+    difficulty: 3,
+    assignees: ["Misha", "Daniel", "Iris"],
+    priority: 1,
+    completed: false,
+  },
+];
 
 export default function AddButton() {
-    const [newTask, setNewTask] = useState( {
-        "description": "",
-        "difficulty": 0 ,
-        "assignees": [
-            
-        ],
-        "priority": 0,
-        "completed": false
-    });
-    const [tasks, setTasks] = useState(taskList);
-    const [open, setOpen] = React.useState(false);
+  const [newTask, setNewTask] = useState({
+    description: "",
+    difficulty: 0,
+    assignees: [],
+    priority: 0,
+    completed: false,
+  });
+  const [tasks, setTasks] = useState(taskList);
+  const [open, setOpen] = React.useState(false);
 
-    const handleAddOpen = (event) => {
-        setOpen(true);
-        console.log(tasks);
-    };
+  const handleAddOpen = ( ) => {
+    setOpen(true);
+    console.log(tasks);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleDescriptionChange = (event) => {
-        const newDescription = event.target.value;
-        setNewTask(prevState => ({
-          ...prevState,
-          ['description']: newDescription
-        }));
-      };
-    
-      const addTask = () => {
-        
-        props.setTasks(prevState => ([
-          ...prevState,
-           newTask
-        ]));
-        props.handleClose();
-      };
+  const handleDescriptionChange = (event) => {
+    const newDescription = event.target.value;
+    setNewTask((prevState) => ({
+      ...prevState,
+      ["description"]: newDescription,
+    }));
+  };
+/*
+  const handleDiffcultyChange = (event) => {
+    const newDiffcuties = event.target.value;
+    setNewDiffcuties((prevState) => ({
+      ...prevState,
+      ["difficulty"]: newDifficulty,
+    }));
+  };
+*/
+  const addTask = () => {
+    setTasks((prevState) => [...prevState, newTask]);
+    handleClose();
+  };
 
-    return(
-        
-      <div>
-          <Button variant="outlined" onClick={handleAddOpen}>
-            Add new task
-          </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-      >
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleAddOpen}>
+        Add new task
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Enter New Task Information</DialogTitle>
         <DialogContent>
           <div className="addEditModal">
@@ -115,7 +101,7 @@ export default function AddButton() {
               label="description"
               type="text"
               variant="standard"
-              />
+            />
           </div>
         </DialogContent>
         <DialogActions>
@@ -123,7 +109,6 @@ export default function AddButton() {
           <Button onClick={addTask}>Add</Button>
         </DialogActions>
       </Dialog>
-      </div>
-    )
-    
+    </div>
+  );
 }
