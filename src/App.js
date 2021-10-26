745739b1e4090bc9c8aa303c15d5e1320eff07a9
digitalsignalprocess <58404228+digitalsignalprocess@users.noreply.github.com>
2021-10-25 20:43:08 -0500

    changes to be fixed

--- src/App.js
+++ src/App.js
@@ -2,17 +2,67 @@ import logo from './logo.svg';
 import './App.css';
 import AddButton from './components/AddButton';
 import TaskCard from "./components/TaskCard";
+import React, { useState} from 'react';
+
+const taskList = [
+  {
+    description: "create task component",
+    difficulty: 3,
+    assignees: ["Jake", "Misha"],
+    priority: 5,
+    completed: false,
+  },
+  {
+    description: "Assign tasks button",
+    difficulty: 3,
+    assignees: ["Iris", "Carina"],
+    priority: 5,
+    completed: false,
+  },
+  {
+    description: "Task List",
+    difficulty: 3,
+    assignees: ["Daniel", "Yijing"],
+    priority: 3,
+    completed: false,
+  },
+  {
+    description: "login",
+    difficulty: 3,
+    assignees: ["Jake", "Carina"],
+    priority: 2,
+    completed: false,
+  },
+  {
+    description: "mood tracking",
+    difficulty: 3,
+    assignees: ["Misha", "Daniel", "Iris"],
+    priority: 1,
+    completed: false,
+  },
+];
+
 
 
 function App() {
+  const [tasks, setTasks] = useState(taskList);
+
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <div className="body">
-        <AddButton/>
+        <AddButton
+          setTasks={setTasks}
+        />
       </div>
-      <TaskCard></TaskCard>
+      {Object.values(tasks).map((task) => ( 
+      <TaskCard>
+        task={task}
+      </TaskCard>
+      ))
+      }
     </div>
   );
 }
