72448eb8a181b523f23693620e61e4edf9bd0497
Misha Ivaniuk <mykhailo.ivaniuk@gmail.com>
2021-10-25 21:11:00 -0500

    Added functionality for Assign Tasks button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,81 @@ import logo from './logo.svg';
 import './App.css';
 import AddButton from './components/AddButton';
 import TaskCard from "./components/TaskCard";
+import AssignButton from './components/assignButton';
 
+let taskList = [
+  {
+      "description": "create task component",
+      "difficulty": 3,
+      "assignees": [
+      ],
+      "priority": 5,
+      "completed": false
+  },
+  {
+      "description": "Assign tasks button",
+      "difficulty": 3,
+      "assignees": [
+      ],
+      "priority": 5,
+      "completed": false
+  },
+  {
+      "description": "Task List",
+      "difficulty": 3,
+      "assignees": [
+
+      ],
+      "priority": 3,
+      "completed": false
+  },
+  {
+      "description": "login",
+      "difficulty": 3,
+      "assignees": [
+
+      ],
+      "priority": 2,
+      "completed": false
+  },
+  {
+      "description": "mood tracking",
+      "difficulty": 3,
+      "assignees": [
+
+      ],
+      "priority": 1,
+      "completed": false
+  },
+]
+
+let mems = [
+  {
+      "name": "Jake",
+      "score": 0
+  }, 
+  {
+      "name": "Misha",
+      "score": 0
+  }, 
+
+  {
+      "name": "Carina",
+      "score": 0
+  },
+  {
+      "name": "Iris",
+      "score": 0
+  }, 
+  {
+      "name": "Daniel",
+      "score": 0
+  }, 
+  {
+      "name": "Yijing",
+      "score": 0
+  }
+]
 
 function App() {
   return (
@@ -11,6 +85,7 @@ function App() {
       </header>
       <div className="body">
         <AddButton/>
+        <AssignButton members={mems} tasks={taskList}/>
       </div>
       <TaskCard></TaskCard>
     </div>
