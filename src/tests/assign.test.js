0d34a0c4caa2427e799dbccd3c36639694dd8090
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2021-10-27 18:44:30 -0500

    skeleton of assign tests

---
+++ src/tests/assign.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import assignTasks from '../Utilities/assign';
+
+test('Tasklist does not change when there are no unassigned tasks', () => {
+
+
+});
+
+test('Tasklist shows assigned members correctly after initial assignment', () => {
+
+})
